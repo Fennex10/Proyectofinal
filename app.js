b99d9ebf5d765e4ee094f7302a157f1c4e6779c2
Fennex10 <dewringiron821@gmail.com>
2025-12-01 14:30:15 -0400

    si

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hola Mundo desde DevOps CI/CD!");
 });
 
-export default app;
-
-// Solo iniciar servidor si NO estamos en pruebas
-if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log("Servidor corriendo en el puerto " + PORT);
-  });
-}
+module.exports = app;
