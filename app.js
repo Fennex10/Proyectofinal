bf2aad39a4dcfc5c47408053e355618d96b8bd56
Fennex10 <dewringiron821@gmail.com>
2025-12-01 13:50:57 -0400

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-function hola() {
-    return "Hola Mundo desde DevOps!";
-}
+const http = require('http');
+
+const hola = () => "Hola Mundo desde DevOps CI/CD!";
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(hola());
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 module.exports = hola;
--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const hola = require('./app');
 
-test("Debe responder Hola Mundo", () => {
-    expect(hola()).toBe("Hola Mundo desde DevOps!");
+test("Debe responder Hola Mundo desde DevOps CI/CD!", () => {
+    expect(hola()).toBe("Hola Mundo desde DevOps CI/CD!");
 });
